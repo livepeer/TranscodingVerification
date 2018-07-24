f61d18b77afa1d8144c3758a99d723907b780cdf
Luc Trudeau <luc@trud.ca>
2018-07-24 16:01:55 -0400

    Sum of Squares Function
    Basic sum of squares function to help test the API structure.

--- src/mea_frame.c
+++ src/mea_frame.c
@@ -2,5 +2,30 @@
  * Copyright 2018 Livepeer
  * SPDX-License-Identifier: MIT
  */
+#include <assert.h>
 
 #include "mea.h"
+
+int64_t sse(const MeaPlane *plane1, const MeaPlane *plane2) {
+    const unsigned height = plane1->height;
+    const unsigned width = plane1->width;
+    // SSE requires the same frame size.
+    assert(height == plane2->height);
+    assert(width == plane2->width);
+
+    const ptrdiff_t p1_str = plane1->stride;
+    const ptrdiff_t p2_str = plane2->stride;
+
+    int64_t result = 0;
+    const uint8_t *p1 = plane1->data;
+    const uint8_t *p2 = plane2->data;
+    for (unsigned y = 0; y < height; y++) {
+        for (unsigned x = 0; x < width; x++) {
+            const int diff = p1[x] - p2[x];
+            result += diff * diff;
+        }
+        p1 += p1_str;
+        p2 += p2_str;
+    }
+    return result;
+}

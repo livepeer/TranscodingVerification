797ae996578ee7df1d2be6ef57c051bc9a08cf2b
Luca Barbato <lu_zero@gentoo.org>
2018-07-27 04:37:53 +0200

    Make the test cover the 0-inf range of psnr

--- test/sse.c
+++ test/sse.c
@@ -3,19 +3,31 @@
  * SPDX-License-Identifier: MIT
  */
 #include <stdio.h>
+#include <string.h>
 
 #include "mea.h"
 #include "mea_internal.h"
 
 int main(void) {
-    uint8_t data[16] = { 242, 129, 25, 10, 99, 33, 111, 137,
+    uint8_t ref[16] = { 242, 129, 25, 10, 99, 33, 111, 137,
                          242, 129, 25, 10, 99, 33, 111, 137 };
-    MeaPlane p1;
-    p1.data = data;
+    uint8_t rec[16] = { 242, 120, 25, 10, 99, 33, 111, 131,
+                         242, 129, 25, 10, 99, 33, 111, 131 };
+    MeaPlane p1, p2;
+    p1.data = ref;
     p1.stride = 4;
     p1.width = 2;
     p1.height = 2;
 
-    printf("%f", psnr(&p1, &p1));
+    p2.data = rec;
+    p2.stride = 4;
+    p2.width = 2;
+    p2.height = 2;
+
+    printf("psnr %f\n", psnr(&p1, &p1));
+    printf("psnr %f\n", psnr(&p1, &p2));
+    memset(rec, 0, sizeof(rec));
+    printf("psnr %f\n", psnr(&p1, &p2));
+
     return 0;
 }
